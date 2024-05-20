b6b8e21db62ddd303280022194f864967209f1e0
sergejmehonosin <sergey01051997@mail.ru>
2024-05-20 10:00:15 +0300

    lesson_10_task_3

---
+++ lesson_10/Task_3.kt
@@ -0,0 +1,32 @@
+package kurs_kotlin_basic.lesson_10
+import java.lang.StringBuilder
+
+/*
+Урок 10 Задача 3
+
+Напиши программу, которая генерирует пароли. Пароль должен состоять из последовательно чередующихся цифр и
+специальных символов. Например, 4#4%2!.
+
+- пользователь сам задает длину пароля;
+- для генерации пароля должна быть отдельная функция, принимающая целочисленное значение длины пароля и
+возвращающая готовый пароль;
+- пароль должен содержать цифры от 0 до 9, специальные символы: !"#$%&'()*+,-./ и пробел (всего 16 спецсимволов).
+
+ */
+
+internal fun main() {
+    println("Введите длину пароля : ")
+    val passwordLength = readln().toUInt()
+    println("Ваш пароль : ${getGeneratePassword(passwordLength)}")
+}
+
+internal fun getGeneratePassword(passwordLength: UInt): String {
+    val intRange: IntRange = 1..9
+    val specialSymbols = "!\"#\$%&'()*+,-./ "
+    val generatedPassword = StringBuilder()
+
+    for (i in 1..passwordLength.toInt() step 2) {
+        generatedPassword.append(intRange.random(), specialSymbols.random())
+    }
+    return generatedPassword.toString()
+}
